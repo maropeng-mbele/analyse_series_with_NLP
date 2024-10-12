9ff376c5b738dfa9eecaf0b6c7cf1c5c477d3678
maropeng-mbele <maropeng.mbele@gmail.com>
2024-10-12 13:19:29 +0200

    change_traning_utils

--- text_classification/training_utils.py
+++ text_classification/training_utils.py
@@ -3,14 +3,16 @@ import numpy as np
 import evaluate
 
 metric = evaluate.load('accuracy')
+
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
 
 def get_class_weights(df):
+    # Convert the classes list to a numpy array
     class_weights = compute_class_weight("balanced",
-                        classes = sorted(df['label'].unique().tolist()),
+                        classes = np.array(sorted(df['label'].unique().tolist())),
                         y = df['label'].tolist()
                         )
     return class_weights
