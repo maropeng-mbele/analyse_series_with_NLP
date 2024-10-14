9ec0b07f0843834730b7626d22a2b190bd6b7c9f
maropeng-mbele <maropeng.mbele@gmail.com>
2024-10-14 12:21:57 +0200

    custom trainer change

--- text_classification/custom_trainer.py
+++ text_classification/custom_trainer.py
@@ -3,21 +3,24 @@ from torch import nn
 from transformers import Trainer
 
 class CustomTrainer(Trainer):
-    def compute_loss(self,model,inputs,return_outputs=False):
+    def compute_loss(self, model, inputs, return_outputs=False):
         labels = inputs.get("labels")
 
         # Forward Pass
         outputs = model(**inputs)
         logits = outputs.get("logits")
-        logits = logits.float()
+        logits = logits.float()  # Ensure logits are in float32
 
-        # Compute Custom Loss
-        loss_fct = nn.CrossEntropyLoss(weight = torch.tensor(self.class_weights).to(device=self.device))
-        loss = loss_fct(logits.view(-1, self.model.config.num_labels),labels.view(-1))
-        return (loss,outputs) if return_outputs else loss
+        # Compute loss with class weights moved to the appropriate device
+        loss_fct = nn.CrossEntropyLoss(weight=torch.tensor(self.class_weights, dtype=torch.float32).to(device=self.device))
+        labels = labels.to(self.device).long()  # Ensure labels are in long (int64) type
+
+        # Compute loss
+        loss = loss_fct(logits.view(-1, self.model.config.num_labels), labels.view(-1))
+        return (loss, outputs) if return_outputs else loss
     
-    def set_class_weights(self,class_weights):
+    def set_class_weights(self, class_weights):
         self.class_weights = class_weights
 
-    def set_device(self,device):
+    def set_device(self, device):
         self.device = device
